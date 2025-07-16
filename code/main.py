b277a6ab41f1c3ca9a61c28daa15f658af1e334c
Jibin Gijo <jibingijo123@gmail.com>
2025-07-16 09:52:23 +0530

    upDATED highscore

--- code/main.py
+++ code/main.py
@@ -1,4 +1,6 @@
 import pygame, sys
+import json
+import os
 from player import Player
 import obstacle
 from alien import Alien, Extra
@@ -17,6 +19,17 @@ class Game:
 		self.live_x_start_pos = screen_width - (self.live_surf.get_size()[0] * 2 + 20)
 		self.score = 0
 		self.font = pygame.font.Font('../font/Pixeled.ttf',20)
+		
+		# Highscore setup
+		self.highscore_file = 'highscore.json'
+		self.highscore_data = self.load_highscore()
+		self.highscore = self.highscore_data.get('score', 0)
+		self.highscore_player = self.highscore_data.get('player', 'Anonymous')
+		self.game_over = False
+		self.new_highscore = False
+		self.entering_name = False
+		self.player_name = ""
+		self.name_input_active = False
 
 		# Obstacle setup
 		self.shape = obstacle.shape
@@ -44,6 +57,66 @@ class Game:
 		self.laser_sound.set_volume(0.5)
 		self.explosion_sound = pygame.mixer.Sound('../audio/explosion.wav')
 		self.explosion_sound.set_volume(0.3)
+
+	def load_highscore(self):
+		"""Load highscore from JSON file"""
+		try:
+			if os.path.exists(self.highscore_file):
+				with open(self.highscore_file, 'r') as f:
+					data = json.load(f)
+					return {
+						'score': data.get('score', 0),
+						'player': data.get('player', 'Anonymous')
+					}
+			else:
+				return {'score': 0, 'player': 'Anonymous'}
+		except (json.JSONDecodeError, IOError):
+			return {'score': 0, 'player': 'Anonymous'}
+
+	def save_highscore(self, player_name):
+		"""Save highscore to JSON file"""
+		try:
+			data = {
+				'score': self.highscore,
+				'player': player_name
+			}
+			with open(self.highscore_file, 'w') as f:
+				json.dump(data, f, indent=2)
+		except IOError:
+			print("Error saving highscore file")
+
+	def check_new_highscore(self):
+		"""Check if current score is a new highscore"""
+		if self.score > self.highscore:
+			self.new_highscore = True
+			self.entering_name = True
+			self.name_input_active = True
+			self.player_name = ""
+			return True
+		return False
+
+	def submit_highscore(self):
+		"""Submit the new highscore with player name"""
+		if self.player_name.strip() == "":
+			self.player_name = "Anonymous"
+		
+		self.highscore = self.score
+		self.highscore_player = self.player_name
+		self.save_highscore(self.player_name)
+		self.entering_name = False
+		self.name_input_active = False
+
+	def handle_name_input(self, event):
+		"""Handle keyboard input for player name"""
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_RETURN:
+				self.submit_highscore()
+			elif event.key == pygame.K_BACKSPACE:
+				self.player_name = self.player_name[:-1]
+			else:
+				# Only allow alphanumeric characters and spaces, max length 15
+				if len(self.player_name) < 15 and (event.unicode.isalnum() or event.unicode == ' '):
+					self.player_name += event.unicode
 
 	def create_obstacle(self, x_start, y_start,offset_x):
 		for row_index, row in enumerate(self.shape):
@@ -131,8 +204,8 @@ class Game:
 					laser.kill()
 					self.lives -= 1
 					if self.lives <= 0:
-						pygame.quit()
-						sys.exit()
+						self.game_over = True
+						self.check_new_highscore()
 
 		# aliens
 		if self.aliens:
@@ -140,8 +213,8 @@ class Game:
 				pygame.sprite.spritecollide(alien,self.blocks,True)
 
 				if pygame.sprite.spritecollide(alien,self.player,False):
-					pygame.quit()
-					sys.exit()
+					self.game_over = True
+					self.check_new_highscore()
 	
 	def display_lives(self):
 		for live in range(self.lives - 1):
@@ -153,22 +226,115 @@ class Game:
 		score_rect = score_surf.get_rect(topleft = (10,-10))
 		screen.blit(score_surf,score_rect)
 
+	def display_highscore(self):
+		highscore_surf = self.font.render(f'highscore: {self.highscore}',False,'white')
+		highscore_rect = highscore_surf.get_rect(topleft = (10,15))
+		screen.blit(highscore_surf,highscore_rect)
+		
+		# Display highscore holder name
+		player_surf = self.font.render(f'by: {self.highscore_player}',False,'yellow')
+		player_rect = player_surf.get_rect(topleft = (10,40))
+		screen.blit(player_surf,player_rect)
+
 	def victory_message(self):
 		if not self.aliens.sprites():
+			if not self.new_highscore:
+				self.check_new_highscore()
 			victory_surf = self.font.render('You won',False,'white')
 			victory_rect = victory_surf.get_rect(center = (screen_width / 2, screen_height / 2))
 			screen.blit(victory_surf,victory_rect)
 
+	def new_highscore_message(self):
+		"""Display new highscore message and name input"""
+		if self.new_highscore and self.entering_name:
+			# New highscore message
+			new_highscore_surf = self.font.render('NEW HIGHSCORE!',False,'gold')
+			new_highscore_rect = new_highscore_surf.get_rect(center = (screen_width / 2, screen_height / 2 - 60))
+			screen.blit(new_highscore_surf,new_highscore_rect)
+			
+			# Score display
+			score_surf = self.font.render(f'Score: {self.score}',False,'white')
+			score_rect = score_surf.get_rect(center = (screen_width / 2, screen_height / 2 - 30))
+			screen.blit(score_surf,score_rect)
+			
+			# Name input prompt
+			prompt_surf = self.font.render('Enter your name:',False,'white')
+			prompt_rect = prompt_surf.get_rect(center = (screen_width / 2, screen_height / 2))
+			screen.blit(prompt_surf,prompt_rect)
+			
+			# Name input box
+			name_color = 'yellow' if self.name_input_active else 'gray'
+			name_surf = self.font.render(f'{self.player_name}_',False,name_color)
+			name_rect = name_surf.get_rect(center = (screen_width / 2, screen_height / 2 + 30))
+			screen.blit(name_surf,name_rect)
+			
+			# Instructions
+			instruction_surf = self.font.render('Press ENTER to submit',False,'gray')
+			instruction_rect = instruction_surf.get_rect(center = (screen_width / 2, screen_height / 2 + 60))
+			screen.blit(instruction_surf,instruction_rect)
+
+	def game_over_message(self):
+		if self.game_over and not self.entering_name:
+			if self.new_highscore:
+				# Show congratulations message
+				congrats_surf = self.font.render('CONGRATULATIONS!',False,'gold')
+				congrats_rect = congrats_surf.get_rect(center = (screen_width / 2, screen_height / 2 - 30))
+				screen.blit(congrats_surf,congrats_rect)
+				
+				highscore_surf = self.font.render(f'New Highscore: {self.highscore}',False,'yellow')
+				highscore_rect = highscore_surf.get_rect(center = (screen_width / 2, screen_height / 2))
+				screen.blit(highscore_surf,highscore_rect)
+				
+				player_surf = self.font.render(f'by {self.highscore_player}',False,'yellow')
+				player_rect = player_surf.get_rect(center = (screen_width / 2, screen_height / 2 + 30))
+				screen.blit(player_surf,player_rect)
+			else:
+				game_over_surf = self.font.render('GAME OVER',False,'white')
+				game_over_rect = game_over_surf.get_rect(center = (screen_width / 2, screen_height / 2))
+				screen.blit(game_over_surf,game_over_rect)
+			
+			restart_surf = self.font.render('Press R to restart',False,'white')
+			restart_rect = restart_surf.get_rect(center = (screen_width / 2, screen_height / 2 + 60))
+			screen.blit(restart_surf,restart_rect)
+
+	def restart_game(self):
+		"""Reset game state for restart"""
+		self.lives = 3
+		self.score = 0
+		self.game_over = False
+		self.new_highscore = False
+		self.entering_name = False
+		self.player_name = ""
+		self.name_input_active = False
+		
+		# Clear all sprite groups
+		self.aliens.empty()
+		self.alien_lasers.empty()
+		self.blocks.empty()
+		self.extra.empty()
+		self.player.sprite.lasers.empty()
+		
+		# Reset player position
+		self.player.sprite.rect.centerx = screen_width / 2
+		
+		# Recreate obstacles and aliens
+		self.create_multiple_obstacles(*self.obstacle_x_positions, x_start = screen_width / 15, y_start = 480)
+		self.alien_setup(rows = 6, cols = 8)
+		self.alien_direction = 1
+		self.extra_spawn_time = randint(40,80)
+
 	def run(self):
-		self.player.update()
-		self.alien_lasers.update()
-		self.extra.update()
+		if not self.game_over:
+			self.player.update()
+			self.alien_lasers.update()
+			self.extra.update()
+			
+			self.aliens.update(self.alien_direction)
+			self.alien_position_checker()
+			self.extra_alien_timer()
+			self.collision_checks()
 		
-		self.aliens.update(self.alien_direction)
-		self.alien_position_checker()
-		self.extra_alien_timer()
-		self.collision_checks()
-		
+		# Always draw everything
 		self.player.sprite.lasers.draw(screen)
 		self.player.draw(screen)
 		self.blocks.draw(screen)
@@ -177,7 +343,10 @@ class Game:
 		self.extra.draw(screen)
 		self.display_lives()
 		self.display_score()
+		self.display_highscore()
 		self.victory_message()
+		self.new_highscore_message()
+		self.game_over_message()
 
 class CRT:
 	def __init__(self):
@@ -213,8 +382,13 @@ if __name__ == '__main__':
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				sys.exit()
-			if event.type == ALIENLASER:
+			if event.type == ALIENLASER and not game.game_over:
 				game.alien_shoot()
+			if event.type == pygame.KEYDOWN:
+				if game.entering_name:
+					game.handle_name_input(event)
+				elif event.key == pygame.K_r and game.game_over:
+					game.restart_game()
 
 		screen.fill((30,30,30))
 		game.run()
